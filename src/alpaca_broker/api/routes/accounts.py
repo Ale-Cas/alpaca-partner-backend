fc4db99262ad1b3bb0c77ac5542c6384ff1ab058
Alessio <castricaalessio@gmail.com>
2023-05-16 09:18:25 +0000

    committing before switching off streamlit

--- src/alpaca_broker/api/routes/accounts.py
+++ src/alpaca_broker/api/routes/accounts.py
@@ -1,10 +1,15 @@
 """Accounts endpoint router."""
 
+import logging
+
 from alpaca.broker import Account, CreateAccountRequest
 from fastapi import APIRouter
 
 from alpaca_broker.api.common import get_broker_client
 from alpaca_broker.enums.api import Routers
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix=Routers.ACCOUNTS.value,
@@ -17,7 +22,6 @@ broker_client = get_broker_client()
 @router.post("/")
 def create_account(
     account_request: CreateAccountRequest,
-    # broker_client: BrokerClient = Depends(get_broker_client),
 ) -> Account:
     """Create an Alpaca account from the account request.
 
@@ -31,6 +35,8 @@ def create_account(
     Account:
         The account that has been created.
     """
+    log.info("Account creation request.")
+    log.info(account_request.dict(exclude_none=True))
     account = broker_client.create_account(account_request)
     assert isinstance(account, Account), "The account has not being parsed for pydantic validation."
     return account
---
+++ tests/api/conftest.py
@@ -0,0 +1,1 @@
+"""Configurations, mocks and fixtures for tests."""
